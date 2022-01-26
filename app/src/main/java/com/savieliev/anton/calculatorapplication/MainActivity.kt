156b5d39a9cfd290678391bac7b2fde2c110f31c
Anton Savieliev <ant.savelev@gmail.com>
2022-01-26 16:23:17 +0200

    creating project

---
+++ app/src/main/java/com/savieliev/anton/calculatorapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.savieliev.anton.calculatorapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
